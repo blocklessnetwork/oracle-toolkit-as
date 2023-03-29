f67fa0239baeb3cddbde1af88c58da874bc53413
Udit <udit@eskaytech.com>
2023-03-29 15:51:38 +0530

    Test for updating price and signature verification

--- contracts/test/01_multiAssetOracle.ts
+++ contracts/test/01_multiAssetOracle.ts
@@ -54,8 +54,33 @@ describe("MultiAssetPriceOracleV1 Data", function () {
     multiAssetOracle = await Oracle.deploy('Test Oracle', 'This is a test orarcle contract', 8)
   })
 
-  it("Should result an empty data", async function () {
+  it("Should result an empty dataset", async function () {
     const [ price ] = await multiAssetOracle.latestData('BNB')
     expect(price).to.equal(BigNumber.from(0))
   })
+
+  it("Should update dataset with price", async function() {
+    const [owner] = await hre.ethers.getSigners()
+    
+    const symbol = 'BNB'
+    const price  = 232 * 1e8
+    const timestamp = Math.floor(Date.now() / 1000)
+
+    // Authorize the updater
+    await multiAssetOracle.authorizeUpdater(owner.address);
+
+    // Sign the message
+    const messageHash = hre.ethers.utils.solidityKeccak256(["string", "uint256", "uint256"], [symbol, price, timestamp])
+    const signature = await owner.signMessage(hre.ethers.utils.arrayify(messageHash));
+
+    await expect(multiAssetOracle.updatePrice(symbol, price, timestamp, signature))
+      .to.emit(multiAssetOracle, "NewAnswer")
+  })
+
+  it("Should result dataset with price", async function () {
+    const matchPrice = 232 * 1e8
+    const [price] = await multiAssetOracle.latestData('BNB')
+    expect(price).to.equal(BigNumber.from(matchPrice))
+  })
+
 })
